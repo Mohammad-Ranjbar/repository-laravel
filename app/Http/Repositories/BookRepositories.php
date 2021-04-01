31932a5b8a08892099136586c284962d806b21a9
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2021-04-01 08:10:45 +0430

    init

---
+++ app/Http/Repositories/BookRepositories.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookRepositories
+{
+    /*
+    * @var Book
+    */
+    public $book;
+
+
+    public function __construct(Book $book)
+    {
+        return $this->book = $book;
+    }
+
+    public function index()
+    {
+        return $this->book->orderBy('created_at', 'desc')->get();
+    }
+
+    public function store()
+    {
+
+    }
+}
